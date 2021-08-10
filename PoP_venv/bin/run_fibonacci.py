5c19c45fe95f5d7ac79aed595f7343c6d8138f09
Nikolai Krokhin <nk1719@ic.ac.uk>
2021-08-10 14:13:03 +0100

    new

---
+++ PoP_venv/bin/run_fibonacci.py
@@ -0,0 +1,7 @@
+#!/Users/nikolaikrokhin/Documents/principles_of_programming/PoP_venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'oo-programming-exercises==2020.0','run_fibonacci.py'
+__requires__ = 'oo-programming-exercises==2020.0'
+__import__('pkg_resources').require('oo-programming-exercises==2020.0')
+__file__ = '/Users/nikolaikrokhin/Documents/principles_of_programming/object-oriented-programming/scripts/run_fibonacci.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
